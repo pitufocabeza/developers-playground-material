914e0b58eae077763ef82c43ee9e9ecb10dce6f9
pitufocabeza <pitufocabeza@gmail.com>
2020-03-09 15:27:13 +0100

    Initial commit

--- data/SiteConfig.js
+++ data/SiteConfig.js
@@ -1,45 +1,45 @@
 const config = {
-  siteTitle: "Gatsby Material Starter", // Site title.
-  siteTitleShort: "GM Starter", // Short site title for homescreen (PWA). Preferably should be under 12 characters to prevent truncation.
-  siteTitleAlt: "GatsbyJS Material Starter", // Alternative site title for SEO.
+  siteTitle: "Developers Playground", // Site title.
+  siteTitleShort: "Dev's Playground", // Short site title for homescreen (PWA). Preferably should be under 12 characters to prevent truncation.
+  siteTitleAlt: "Developer's Playground", // Alternative site title for SEO.
   siteLogo: "/logos/logo-1024.png", // Logo used for SEO and manifest.
-  siteUrl: "https://vagr9k.github.io", // Domain of your website without pathPrefix.
-  pathPrefix: "/gatsby-material-starter", // Prefixes all links. For cases when deployed to example.github.io/gatsby-material-starter/.
+  siteUrl: "https://developers-playground.com", // Domain of your website without pathPrefix.
+  pathPrefix: "/developers-playground-material", // Prefixes all links. For cases when deployed to example.github.io/gatsby-material-starter/.
   fixedFooter: false, // Whether the footer component is fixed, i.e. always visible
-  siteDescription: "A GatsbyJS stater with Material design in mind.", // Website description used for RSS feeds/meta description tag.
+  siteDescription: "A blog about programming, computer science and engineering.", // Website description used for RSS feeds/meta description tag.
   siteRss: "/rss.xml", // Path to the RSS file.
-  siteRssTitle: "Gatsby Material Starter RSS feed", // Title of the RSS feed
-  siteFBAppID: "1825356251115265", // FB Application ID for using app insights
-  siteGATrackingID: "UA-47311644-4", // Tracking code ID for google analytics.
-  disqusShortname: "https-vagr9k-github-io-gatsby-material-starter", // Disqus shortname.
+  siteRssTitle: "Developers Playground RSS feed", // Title of the RSS feed
+  siteFBAppID: "", // FB Application ID for using app insights
+  siteGATrackingID: "UA-41876962-3", // Tracking code ID for google analytics.
+  disqusShortname: "https-developers-playground", // Disqus shortname.
   dateFromFormat: "YYYY-MM-DD", // Date format used in the frontmatter.
   dateFormat: "DD/MM/YYYY", // Date format for display.
-  userName: "Material User", // Username to display in the author segment.
-  userEmail: "MaterialUser@example.com", // Email used for RSS feed's author segment
-  userTwitter: "", // Optionally renders "Follow Me" in the UserInfo segment.
-  userLocation: "North Pole, Earth", // User location to display in the author segment.
-  userAvatar: "https://api.adorable.io/avatars/150/test.png", // User avatar to display in the author segment.
+  userName: "Sascha", // Username to display in the author segment.
+  userEmail: "sascha@developers-playground.com", // Email used for RSS feed's author segment
+  userTwitter: "pitufocabeza1", // Optionally renders "Follow Me" in the UserInfo segment.
+  userLocation: "Munich, Germany", // User location to display in the author segment.
+  userAvatar: "https://pbs.twimg.com/profile_images/1236709748483002369/CBX-rO_5_200x200.jpg", // User avatar to display in the author segment.
   userDescription:
-    "Yeah, I like animals better than people sometimes... Especially dogs. Dogs are the best. Every time you come home, they act like they haven't seen you in a year. And the good thing about dogs... is they got different dogs for different people.", // User description to display in the author segment.
+    "Hi, I'm Sascha, a young professional industrial engineer working in the aerospace industry. Computer Science, Web Development and Programming are my hobbies.", // User description to display in the author segment.
   // Links to social profiles/projects you want to display in the author segment/navigation bar.
   userLinks: [
     {
       label: "GitHub",
-      url: "https://github.com/Vagr9K/gatsby-material-starter",
+      url: "https://github.com/pitufocabeza/developers-playground-material",
       iconClassName: "fa fa-github"
     },
     {
       label: "Twitter",
-      url: "https://twitter.com/Vagr9K",
+      url: "https://twitter.com/pitufocabeza1",
       iconClassName: "fa fa-twitter"
     },
     {
       label: "Email",
-      url: "mailto:vagr9k@gmail.com",
+      url: "mailto:sascha@developers-playground.com",
       iconClassName: "fa fa-envelope"
     }
   ],
-  copyright: "Copyright © 2019. Material User" // Copyright string for the footer of the website and RSS feed.
+  copyright: "Copyright © 2020. Developers Playground" // Copyright string for the footer of the website and RSS feed.
 };
 
 // Validate
